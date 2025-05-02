a3be4f76d8cfb3bf20c21322ffd2a5816729a19c
Tauhid hossen <tauhidhossen04@gmail.com>
2025-05-02 12:29:12 +0600

    use nullish openator in undefine type variable

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -22,6 +22,31 @@
     const result2 = isAuthenticated ? isAuthenticated: 'guest'
     console.log({result2});
     
+    //use optional chaining also nullish operator
+
+    type User = {
+        name: string;
+        address:{
+            city: string;
+            road: string;
+            presentAddress: string;
+            permanentAddress?: string; // use optional type operator '?'
+        }
+    }
+
+
+    const user: User = {
+        name: 'Amir',
+        address:{
+            city: 'Dhaka',
+            road: 'Savar',
+            presentAddress:'Khagan'
+        }
+    }
+    //use nullish operator also optional chaining
+    const permanentAddress = user?.address?.permanentAddress ?? 'No permanent Address'
+    console.log({permanentAddress});
+    
     
 
     //
