209010940b4d8dc6ecb3291cfd319a47f2a0922d
Tauhid hossen <tauhidhossen04@gmail.com>
2025-05-04 23:56:10 +0600

    Lear in grad also typeof

---
+++ module3/3.3.ts
@@ -0,0 +1,55 @@
+{
+    //
+ // type guard use ny typeof operator
+
+ const add = (param1: string | number, param2: string | number) : 
+ string | number =>{
+ 
+    if( typeof param1 === 'number' &&  typeof param2 === 'number'){
+        return param1+ param2
+    }else {
+        return param1.toString() + param2.toString()
+    }
+
+     
+ }
+
+ const result1 = add ('2',4)
+ console.log(result1);
+ 
+// in gard
+
+type NormalUser = {
+    name: string;
+
+}
+type AdminUser = {
+    name: string;
+    role:'admin';
+}
+
+const getUser = (user : NormalUser | AdminUser) =>{
+    if('role' in user ){
+        console.log(`My name is ${user.name} and My role is ${user.role}`);
+        
+    }else{
+        console.log(`My name is ${user.name} `);
+        
+    }
+}
+
+const normalUser :NormalUser ={
+    name:'Tauhid'
+    
+}
+
+const adminUser: AdminUser ={
+    name:"Amir",
+    role:"admin"
+}
+
+getUser(adminUser)
+getUser(normalUser)
+
+    //
+}
